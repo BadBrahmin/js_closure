c9aac84c55cb1ac48b9fd7210cb125fb37df7470
BadBrahmin <tejasshree95@gmail.com>
2019-07-23 19:00:10 +0530

    add HOF exercises

--- higher-order-functions/exercise.js
+++ higher-order-functions/exercise.js
@@ -1,24 +1,47 @@
 // Challenge 1
-function addTwo(num) {}
+function addTwo(num) {
+    console.log(num + 2);
+}
 
 // To check if you've completed it, uncomment these console.logs!
-// console.log(addTwo(3));
-// console.log(addTwo(10));
+console.log(addTwo(3));
+console.log(addTwo(10));
 
 // Challenge 2
-function addS(word) {}
+function addS(word) {
+    console.log(word + "s")
+}
 
 // uncomment these to check your work
-// console.log(addS('pizza'));
-// console.log(addS('bagel'));
+console.log(addS('pizza'));
+console.log(addS('bagel'));
 
 // Challenge 3
-function map(array, callback) {}
+function map(array, callback) {
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(callback(array[i]))
+    }
+    return newArray;
+}
 
-// console.log(map([1, 2, 3], addTwo));
+
+console.log(map([1, 2, 3], addTwo));
 
 // Challenge 4
-function forEach(array, callback) {}
+var numbers = '';
+function forEach(array, callback) {
+    for (var i = 0; i < array.length; i++) {
+        callback(array[i]);
+    };
+    console.log(numbers);
+}
+
+function callback(numb) {
+    numbers += numb;
+}
+
+forEach(['1', '2', '3'], callback); //abc output
 
 // see for yourself if your forEach works!
 
@@ -27,31 +50,31 @@ function forEach(array, callback) {}
 //--------------------------------------------------
 
 //Extension 1
-function mapWith(array, callback) {}
+function mapWith(array, callback) { }
 
 //Extension 2
-function reduce(array, callback, initialValue) {}
+function reduce(array, callback, initialValue) { }
 
 //Extension 3
-function intersection(arrays) {}
+function intersection(arrays) { }
 
 // console.log(intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20]));
 // should log: [5, 15]
 
 //Extension 4
-function union(arrays) {}
+function union(arrays) { }
 
 // console.log(union([5, 10, 15], [15, 88, 1, 5, 7], [100, 15, 10, 1, 5]));
 // should log: [5, 10, 15, 88, 1, 7, 100]
 
 //Extension 5
-function objOfMatches(array1, array2, callback) {}
+function objOfMatches(array1, array2, callback) { }
 
 // console.log(objOfMatches(['hi', 'howdy', 'bye', 'later', 'hello'], ['HI', 'Howdy', 'BYE', 'LATER', 'hello'], function(str) { return str.toUpperCase(); }));
 // should log: { hi: 'HI', bye: 'BYE', later: 'LATER' }
 
 //Extension 6
-function multiMap(arrVals, arrCallbacks) {}
+function multiMap(arrVals, arrCallbacks) { }
 
 // console.log(multiMap(['catfood', 'glue', 'beer'], [function(str) { return str.toUpperCase(); }, function(str) { return str[0].toUpperCase() + str.slice(1).toLowerCase(); }, function(str) { return str + str; }]));
 // should log: { catfood: ['CATFOOD', 'Catfood', 'catfoodcatfood'], glue: ['GLUE', 'Glue', 'glueglue'], beer: ['BEER', 'Beer', 'beerbeer'] }
